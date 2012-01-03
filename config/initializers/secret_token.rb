8b47f5074bba92e859582e3837d79ed04e3c0123
pathe.sene <pathe.sene@xarala.sn>
2012-01-03 23:54:46 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Xaima::Application.config.secret_token = '5f4684d80db1f514ca200f7eb09e6d9b177271729913f004fb63042acca6deb4bea0be369d696cbd80ec33d3223ba4dad7bb68045b1324927e93cdce33629d73'
